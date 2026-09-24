f518df2d95ab6ed23e7d667f00699e8431d8afda
InakiFernandez <77704385+InakiFernandez97@users.noreply.github.com>
2026-09-24 14:03:44 +0000

    cambio en directorio

--- AV_AM_Capstone_004D/Fase 2/Evidencias Proyecto/Frontend/panoramapp/lib/main.dart
+++ AV_AM_Capstone_004D/Fase 2/Evidencias Proyecto/Frontend/panoramapp/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PanoramAPP',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
